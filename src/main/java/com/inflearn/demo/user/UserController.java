3a02a385ba34461489cba20b6291b268094d2894
goseungjo <rhtmdwh@naver.com>
2022-12-19 18:04:43 +0900

    인프런 공부

---
+++ src/main/java/com/inflearn/demo/user/UserController.java
@@ -0,0 +1,56 @@
+package com.inflearn.demo.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> allUser(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User selectUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null){
+            throw new UserNotFoundException(String.format("ID[%S] not found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Validated @RequestBody User user){
+        User createUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User deleteUser = service.deleteById(id);
+
+        if(deleteUser == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+    }
+}
